dddbe327f8b69a1a7f661b2397599553bfcaa00e
zhouyijin <zhouyijin@yxt.com>
2023-08-15 14:50:19 +0800

    延时任务的传递范围

--- src/main/java/io/github/spitmaster/zdelayed/annotation/Zdelayed.java
+++ src/main/java/io/github/spitmaster/zdelayed/annotation/Zdelayed.java
@@ -1,5 +1,7 @@
 package io.github.spitmaster.zdelayed.annotation;
 
+import io.github.spitmaster.zdelayed.enums.DelayedTaskScope;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -14,12 +16,16 @@ import java.lang.annotation.Target;
  * 使用 @DelayTime 标记在方法的参数上用于表示延时多长时间进行执行
  * 如果没有 @DelayTime 则不当做延时任务处理
  *
- * @see DelayTime
  * @author zhouyijin
+ * @see DelayTime
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Zdelayed {
 
-
+    /**
+     * 延时任务的传递范围
+     * 默认是本实例去执行延时任务
+     */
+    DelayedTaskScope taskScope() default DelayedTaskScope.STANDALONE;
 }
---
+++ src/main/java/io/github/spitmaster/zdelayed/enums/DelayedTaskScope.java
@@ -0,0 +1,26 @@
+package io.github.spitmaster.zdelayed.enums;
+
+/**
+ * 延时任务传递的范围
+ *
+ * @author zhouyijin
+ */
+public enum DelayedTaskScope {
+
+    /**
+     * 默认延时任务是单机的
+     */
+    STANDALONE,
+
+    /**
+     * 在使用同一个redis集群的延时任务的服务之间传递任务
+     */
+    REDIS_CLUSTER,
+
+    /**
+     * 通过MQ的方式传递任务
+     * not implemented
+     */
+    MQ,
+
+}
